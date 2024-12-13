101b3599c6c073b5f8ed44f5ccd673b5971187a9
Nikola <nikola.milinkovic90@gmail.com>
2024-12-13 20:43:45 +0100

    react setup

---
+++ src/web/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-blue-600 to-purple-500 p-10">
+      <p className="text-white/80 text-xl font-semibold">
+        Once upon a time in the world of game development,
+        there were amazing game engines that brought dreams to life.
+        Unity, with its versatility,
+        allowed developers to create both 2D and 3D games with ease.
+        Unreal Engine, known for its stunning graphics,
+        powered some of the most visually impressive games ever made.
+        Godot, an open-source gem, provided a flexible and friendly environment for indie developers.
+        And then there was Babylon.js, a powerful engine for creating stunning web-based games and experiences.
+        These engines, each with their unique strengths, empowered creators to build worlds beyond imagination.
+      </p>
+    </div>
+  );
+}
+
+export default App;
